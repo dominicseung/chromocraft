9fd1285c01f4953df668b0b59d85cfced1242ca6
DSNS <48170013+dsnsgithub@users.noreply.github.com>
2020-12-18 22:31:56 -0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,42 @@
+function includeHTML() {
+	var z, i, elmnt, file, xhttp;
+	//loop through a collection of all HTML elements:
+	z = document.getElementsByTagName("*");
+	for (i = 0; i < z.length; i++) {
+		elmnt = z[i];
+		//search for elements with a certain attribute:
+		file = elmnt.getAttribute("include-html");
+		if (file) {
+			//make an HTTP request using the attribute value as the file name:
+			xhttp = new XMLHttpRequest();
+			xhttp.onreadystatechange = function() {
+				if (this.readyState == 4) {
+					if (this.status == 200) {
+						elmnt.innerHTML = this.responseText;
+					}
+					if (this.status == 404) {
+						elmnt.innerHTML = "Page not found.";
+					}
+					//remove the attribute, and call this function once more:
+					elmnt.removeAttribute("include-html");
+					includeHTML();
+				}
+			};
+			xhttp.open("GET", file, true);
+			xhttp.send();
+			//exit the function:
+			return;
+		}
+	}
+}
+
+function GMTFinder() {
+	var d = new Date();
+	var n = d.getTimezoneOffset();
+	var t = (n / 60) * -1;
+	if (t > 0) {
+		t = `+${t}`;
+	}
+	document.getElementById("time").innerHTML = t;
+}
+
